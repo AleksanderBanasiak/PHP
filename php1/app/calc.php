c7e465a2ae700bd78b9cb75e3743299780b9329d
AleksanderBanasiak <olek.banasiak0@gmail.com>
2024-03-18 15:59:29 +0100

    php1

---
+++ php1/app/calc.php
@@ -0,0 +1,63 @@
+<?php
+require_once dirname(__FILE__).'/../config.php';
+
+$amount = null;
+$years = null;
+$interest = null;
+$result = null;
+$messages = array();
+
+
+function getParams(&$amount,&$years,&$interest){
+	$amount = isset($_REQUEST['amount']) ? $_REQUEST['amount'] : null;
+	$years = isset($_REQUEST['years']) ? $_REQUEST['years'] : null;
+	$interest = isset($_REQUEST['interest']) ? $_REQUEST['interest'] : null;
+}
+
+function validate(&$amount,&$years,&$interest,&$messages){
+	if ( ! (isset($amount) && isset($years) && isset($interest))) return false;
+	
+	if ( $amount == "") $messages [] = 'Nie podano kwoty';
+	
+	if ( $years == "") $messages [] = 'Nie podano na ile lat chcesz wziąć kredyt';
+	
+	if ( $interest == "") $messages [] = 'Nie podano oprocentowania';
+	
+	if (count ( $messages ) != 0) return false;
+	
+	if (! is_numeric( $amount )) $messages [] = 'Kwota nie jest liczbą całkowitą';
+
+	if ( $amount < 0 ) $messages [] = 'Kwota jest ujemna';
+	
+	if (! is_numeric( $years )) $messages [] = 'Lata kredytu nie są liczbą całkowitą';
+
+	if ( $years < 0 ) $messages [] = 'Lata są mniejsze od zera';
+		
+	if (! is_numeric( $interest )) $messages [] = 'Oprocentowanie nie jest liczbą całkowitą';
+
+	if ( $interest < 0 ) $messages [] = 'Oprocentowanie jest ujemne';
+	
+	if (count ( $messages ) != 0) return false;
+
+	else return true;
+}
+
+function calculate(&$amount,&$years,&$interest,&$result){
+
+	$amount = intval($amount);
+	$years = intval($years);
+	$interest = intval($interest);
+
+	$result = round($amount/ (12* $years) + (($amount* ($interest/100) )/12 ));
+	
+}
+
+
+getParams($x,$y,$operation);
+if (validate($x,$y,$operation,$messages) ) { 
+	calculate($x,$y,$operation,$result);
+}
+
+
+include 'calc_view.php';
+?>
---
+++ php1/app/calc_view.php
@@ -0,0 +1,44 @@
+<?php require_once dirname(__FILE__) .'/../config.php';?>
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator kredytowy</title>
+</head>
+<body>
+<h1>Kalkulator kredytowy</h1>
+<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
+	<label for="amount">Kwota: </label>
+	<input id="amount" type="text" name="amount" value="<?php checkIfIsSet($amount); ?>" />
+	<br />
+	<br />
+	
+	<label for="years">Na ile lat: </label>
+	<input id="years" type="text" name="years" value="<?php checkIfIsSet($years); ?>" />
+	<br />
+	<br />
+
+	<label for="interest">Oprocentowanie: </label>
+	<input id="interest" type="text" name="interest" value="<?php checkIfIsSet($interest); ?>" />
+	<br />
+	<br />
+
+	<input type="submit" value="Oblicz" />
+</form>	
+
+<?php
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+<?php echo 'Wynik: '.$result; ?>
+
+</body>
+</html>
